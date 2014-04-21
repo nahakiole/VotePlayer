742164af7e13adf0160a749ff6b49f8d99334e04
haesler <jm7kccUT>
2014-04-21 15:03:11 +0200

    Added Usermanagement.

--- Controller/User.php
+++ Controller/User.php
@@ -10,10 +10,21 @@ namespace Controller;
 
 
 use Fredy\Controller\Controller;
+use Fredy\Model\Repository\Condition;
+use Fredy\Model\Repository\Filter;
 use Fredy\View\HTMLResponse;
+use Model\Repository\JournalRepository;
+use Model\Repository\UserRepository;
 
 class User extends Controller
 {
+    private $db;
+
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
+
 
     /**
      * @param $request \Fredy\Model\Entity\Request
@@ -22,9 +33,12 @@ class User extends Controller
     function indexAction($request)
     {
         $response = new HTMLResponse('users.twig');
+        $UserRepository = new UserRepository($this->db);
+        $user = $UserRepository->findAll(10);
         $navigation = new Navigation('navigation.json');
         $response->setTwigVariables([
-                'navigation' => $navigation->getNavigation($request->matches[0])
+                'navigation' => $navigation->getNavigation($request->matches[0]),
+                'users' => $user
             ]
         );
         return $response;
@@ -52,9 +66,31 @@ class User extends Controller
     function editAction($request)
     {
         $response = new HTMLResponse('user.twig');
+        $UserRepository = new UserRepository($this->db);
+        if(isset($request->matches["userid"])) {
+            $userID = $UserRepository->findById($request->matches["userid"]);
+        }
+        else
+        {
+            $userID = null;
+        }
+
+        if(isset($request->POST["submit"])){
+            if(isset($userID)) {
+                $userupdate = new \Model\Entity\User($request->matches["userid"],$request->POST["username"],password_hash($request->POST["password"], PASSWORD_DEFAULT));
+                $UserRepository->update($userupdate);
+            }
+            else
+            {
+                $usercreate = new \Model\Entity\User(null,$request->POST["username"],password_hash($request->POST["password"], PASSWORD_DEFAULT));
+                $UserRepository->create($usercreate);
+            }
+        };
+
         $navigation = new Navigation('navigation.json');
         $response->setTwigVariables([
-                'navigation' => $navigation->getNavigation($request->matches[0])
+                'navigation' => $navigation->getNavigation($request->matches[0]),
+                'user' => $userID
             ]
         );
         return $response;
